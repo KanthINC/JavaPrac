277d8e0952b0ae6f07f7335120147bbbe3badf6b
ramakanth.b <ramakanth.b@BLR-NB0020>
2018-04-09 19:15:14 +0530

    TestDummy

--- JavaPractise/src/in/kanth/Test/HelloTest.java
+++ JavaPractise/src/in/kanth/Test/HelloTest.java
@@ -5,6 +5,25 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("My git first");
 		System.out.println("New Branch Ramakanth");
+		
+		int i=2;
+		int j=2;
+		boolean flag=true;
+		while(flag){
+
+		   int temp1=i*i*i;
+		   while(i<j){
+		   int temp2=j*j;
+		   if(temp1==temp2){
+			   System.out.println(temp1 +" --- "+temp2);
+			   flag=false;
+			   break;
+			  
+		   }
+		    j++;
+		   }
+			i++;
+		}
 	}
 
 }
