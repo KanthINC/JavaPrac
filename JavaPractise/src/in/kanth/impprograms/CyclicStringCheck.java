f19a12f01b2b6bcb86a9108995ff66f59308cc31
ramakanth.b <ramakanth.b@BLR-NB0020>
2019-03-07 11:28:22 +0530

    important programs

---
+++ JavaPractise/src/in/kanth/impprograms/CyclicStringCheck.java
@@ -0,0 +1,29 @@
+package in.kanth.impprograms;
+
+/**
+ * 
+ * @author ramakanth.b 
+ * Des: find whether the string rotated is from actual
+ *         string
+ */
+public class CyclicStringCheck {
+
+	public static void main(String[] args) {
+
+		String actual = "Ramakanth";
+		String rotated = "akanthRam";
+
+		boolean check = check(actual, rotated);
+		System.out.println(check ? "Its is roatated string" : "Not from actual string");
+
+	}
+
+	private static boolean check(String actual, String rotated) {
+		String mytempString = rotated + rotated;
+		if (mytempString.contains(actual)) {
+			return true;
+		}
+		return false;
+	}
+
+}
